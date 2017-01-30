6cd0cb2e798ff874cf7b3d3760effb8574902ca7
weifengshi <shiweifeng@live.com>
2017-01-30 13:04:38 -0600

    Add files via upload

---
+++ CL2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>;
+#include<string>;
+using namespace std;
+
+int main() {
+	float ratio;
+	cout << "How many batches?";
+	cin >> ratio;
+	
+	
+	float a = ratio;
+	float b = .5*ratio;
+	float c = ratio;
+	float d = 1.5*ratio;
+	float e = ratio;
+	cout << a << " tsp baking soda" << endl;
+	cout << b << " tsp baking powder" << endl;
+	cout << c << " c butter" << endl;
+	cout << d << " c white sugar" << endl;
+	cout << e << " egg" << endl;
+	while (true);
+	return 0;
+}
