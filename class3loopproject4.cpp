eeda1a28aba45e31a177a9cd65a0c35d4e0b817b
weifengshi <shiweifeng@live.com>
2018-09-14 10:45:40 -0700

    Update class3loopproject4.cpp
    111

--- class3loopproject4.cpp
+++ class3loopproject4.cpp
@@ -3,31 +3,9 @@ using namespace std;
 
 int main()
 {
-	float startingWage;
-	float percentRaisePerYear;
-	int yearsWorked;
-
-	cout << "What is your starting wage? >> ";
-	cin >> startingWage;
-
-	cout << "What % of a raise do you get per year? >> %";
-	cin >> percentRaisePerYear;
-
-	percentRaisePerYear /= 100;
-
-	cout << "How many years have you worked? >> ";
-	cin >> yearsWorked;
-
-	float adjustedWage = startingWage;
-
-	for (int i = 0; i < yearsWorked; i+=3)
-	{
-		adjustedWage += adjustedWage * percentRaisePerYear;
-	}
-
-	cout << "Your new wage is $" << adjustedWage << endl;
-
-	int x;
-	cin >> x;
+	int x = 2;
+	cout << x;
+	
+		
 	return 0;
 }
